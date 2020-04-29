c287765aef0e14a66af7bc1e04809ee12a96fe8d
AMIT SINGH RAWAT <53403458+Amitrawat12@users.noreply.github.com>
2020-04-29 21:27:27 +0530

    Add files via upload
    A image to voice node converter which generally use GTTS and OCR for achieving the goal. but it also fetches the noise data.

---
+++ tml.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\rawat\AppData\Local\Programs\Python\Python36-32\Lib\site-packages\pyqt5_tools\tml.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowModality(QtCore.Qt.NonModal)
+        MainWindow.resize(650, 542)
+        MainWindow.setDockOptions(QtWidgets.QMainWindow.AllowTabbedDocks|QtWidgets.QMainWindow.AnimatedDocks)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 100, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(176, 30, 291, 181))
+        self.label.setFrameShape(QtWidgets.QFrame.Box)
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(176, 259, 291, 161))
+        self.label_2.setFrameShape(QtWidgets.QFrame.Box)
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(64, 300, 91, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(230, 460, 181, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 650, 21))
+        self.menubar.setDefaultUp(False)
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.pushButton.clicked.connect(self.setImage)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "select"))
+        self.pushButton_2.setText(_translate("MainWindow", "Press to convert"))
+        self.pushButton_3.setText(_translate("MainWindow", "Press to mp3 file"))
+
+    def setImage(self):
+        fileName, _=QtWidgets.QFileDialog.getOpenFileName(None, "Select Image","", "Image File(*.png *.jpg *jpeg *.bmp)")
+        if fileName:
+            pixmap = QtGui.QPixmap(fileName)
+            pixmap = pixmap.scaled(self.label.width(),self.label.height(),QtCore.Qt.KeepAspectRatio)
+            self.label.setPixmap(pixmap)
+            self.label.setAlignment(QtCore.Qt.AlignCenter)
+            
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
